0b78179eb876559f97b7325e37f60e18f4f86689
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-03-06 00:43:03 +0600

    fully done

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,24 @@ import Cetagory from './Cetagory'
 
 const App = () => {
   const [items, setItems] = useState(Data);
+  const [activeButton, setActiveButton] = useState(null)
 
   const filterItem = (category) => {
     const newItems = Data.filter((newVal) => newVal.category === category);
     setItems(newItems);
+    setActiveButton(category)
   };
   const categories = [...new Set(Data.map((product) => product.category))];
   const resetItems = () => {
     setItems(Data);
+    setActiveButton(Data)
   };
 
   return (
     <section>
       <h2 className='text-[48px] text-center font-bold text-orange-400'>Category Filter App</h2>
       <div className="max-w-container mx-auto">
-        <Cetagory categories={categories} filterItem={filterItem} resetItems={resetItems} />
+        <Cetagory activeButton={activeButton} categories={categories} filterItem={filterItem} resetItems={resetItems}  />
         <Product items={items} />
       </div>
     </section>
--- src/Cetagory.jsx
+++ src/Cetagory.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 import Data from './Data'
 
-const Cetagory = ({categories, filterItem, setItems}) => {
+const Cetagory = ({categories, filterItem, resetItems, activeButton}) => {
 
   return (
     <>
         <div className="flex gap-5 my-10 justify-center items-center">
             {
                 categories.map(item=>(
-                    <button className='text-3xl capitalize bg-orange-500 py-4 px-8 rounded-xl' onClick={() => filterItem(item)}>{item}</button>
+                    <button className={`text-3xl capitalize  py-4 px-8 rounded-xl ${activeButton === item ? "bg-slate-400": "bg-orange-500"}`} onClick={() => filterItem(item)}>{item}</button>
                 ))
             }
-            <button className='text-3xl capitalize bg-orange-500 py-4 px-8 rounded-xl'onClick={() => setItems(Data)}>All</button>
+            <button className={`text-3xl capitalize  py-4 px-8 rounded-xl ${activeButton === Data ? "bg-slate-400": "bg-orange-500"}`} onClick={resetItems}>All</button>
         </div>
     
     </>
